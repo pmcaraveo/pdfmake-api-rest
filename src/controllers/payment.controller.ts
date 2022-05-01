44523e5e341a628bba3b0c962db544dd02e5fab1
PabloCaraveo <52939486+pmcaraveo@users.noreply.github.com>
2022-04-30 20:50:42 -0500

    updating

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -2,11 +2,10 @@ import e, { Request, Response } from 'express';
 import { FileBase64, StudentPaymentData } from '../interface';
 import { pdfCONVENIO, pdfREFERENCE } from '../shared/functions-doc';
 import { createBase64 } from '../shared/write-file-pdf';
-import axios from 'axios';
-const formatCurrency = require('format-currency');
+import  axios from 'axios';
 
 export async function getPaymentDocument(req: Request, res: Response): Promise<Response> {
-   /*const reference = req.params['reference'];
+   const reference = req.params['reference'];
    const type = req.params['type'];
    const datos: any  = {
       reference, type
@@ -15,12 +14,11 @@ export async function getPaymentDocument(req: Request, res: Response): Promise<R
    const alumno_data = await axios.post<any>('https://v7-dot-api-rest-dot-cecytec.appspot.com/api/pdf/query', datos)
         .then(e => e.data);
 
-   console.log(alumno_data);*/
-   const studentPaymentData: StudentPaymentData = req.body;
+   console.log(alumno_data);
+   const studentPaymentData: StudentPaymentData = alumno_data;// req.body;
    
    let pdf: any;
    studentPaymentData.products = JSON.parse(studentPaymentData.productos)
-   console.log( studentPaymentData.products[0]);
    if(Number(studentPaymentData.type) == 1 ) {
       pdf = pdfCONVENIO(studentPaymentData)
    } else if(Number(studentPaymentData.type) == 2) {
